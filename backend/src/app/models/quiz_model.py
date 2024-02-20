2060c736596c0479a623d48c1bf054177c1c2c3f
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2024-02-20 20:10:01 +0500

    auth & quiz init

---
+++ backend/src/app/models/quiz_model.py
@@ -0,0 +1,16 @@
+from uuid import UUID
+from sqlmodel import Field, SQLModel
+from app.models.base_uuid_model import BaseUUIDModel
+from datetime import datetime, timedelta
+
+
+class QuizBase(SQLModel):
+    title: str = Field(index=True)
+    creator_id: UUID
+    time_limit: timedelta | None = Field(default=None)
+
+
+
+class Quiz(BaseUUIDModel, QuizBase, table=True):
+    pass
+
