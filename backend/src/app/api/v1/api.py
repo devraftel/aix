e291b8183fe4c125d5ac1ee51d267fc58180c301
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2024-02-22 17:33:04 +0500

    chore: add database and api health check

--- backend/src/app/api/v1/api.py
+++ backend/src/app/api/v1/api.py
@@ -8,7 +8,11 @@ from app.api.v1.endpoints import (
 
 api_router = APIRouter()
 
-api_router.include_router(quiz_engine.router, prefix="/quiz", tags=["Quiz Engine"])
-api_router.include_router(quiz_attempt.router, prefix="/quiz_attempt", tags=["Quiz Attempt"])
-api_router.include_router(user_file.router, prefix="/user_file", tags=["User File"])
-api_router.include_router(questions_engine.router, prefix="/questions", tags=["Questions Engine "])
+api_router.include_router(
+    quiz_engine.router, prefix="/quiz", tags=["Quiz Engine"])
+api_router.include_router(
+    quiz_attempt.router, prefix="/quiz_attempt", tags=["Quiz Attempt"])
+api_router.include_router(
+    user_file.router, prefix="/user_file", tags=["User File"])
+# This was just to test Questions Crud - we will the endpoints file later
+# api_router.include_router(questions_engine.router, prefix="/questions", tags=["Questions Engine "])
