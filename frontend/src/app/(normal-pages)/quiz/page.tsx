62354632a4a6743f27a789ed7d4c917b5639efcb
Muhammad Ahmad <ahmadshaukat_4@outlook.com>
2024-02-22 23:28:41 +0500

    Add quiz list page, dropdown menu and table component

--- frontend/src/app/(normal-pages)/quiz/page.tsx
+++ frontend/src/app/(normal-pages)/quiz/page.tsx
@@ -1,10 +1,13 @@
 import MaxWidthWrapper from '@/components/max-width-wrapper';
+import { QuizListTable } from './_components/table/data-table';
 
 export default function QuizList() {
 	return (
 		<>
-			<MaxWidthWrapper className='mb-12 mt-14 sm:mt-20 flex flex-col items-center justify-center text-center'>
-				Quiz List
+			<MaxWidthWrapper className='mb-12 mt-14 sm:mt-20'>
+				<h1 className='font-semibold text-2xl text-left mb-4'>Quiz List</h1>
+				<hr className='mb-6 w-[20%]' />
+				<QuizListTable />
 			</MaxWidthWrapper>
 		</>
 	);
