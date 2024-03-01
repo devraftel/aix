40dee8a84068abd79b92fb54d49838bbec87ce15
Muhammad Ahmad <ahmadshaukat_4@outlook.com>
2024-03-01 11:49:08 +0500

    Update page layout and add validation for file upload

--- frontend/src/app/(main)/sign-in/[[...sign-in]]/page.tsx
+++ frontend/src/app/(main)/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,7 @@ import { SignIn } from '@clerk/nextjs';
 export default function Page() {
 	return (
 		<MaxWidthWrapper className='mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center'>
-			<SignIn />
+			<SignIn afterSignInUrl={'/quiz'} />
 		</MaxWidthWrapper>
 	);
 }
